0659734b0e1cef0efd014c2e2e3855a323fbb8ca
shyamini <nskshyamini@gmail.com>
2019-06-01 14:52:02 -0700

    :construction: :zap: Add polynomial integration over unit triangle

--- src/polynomial_integral.cc
+++ src/polynomial_integral.cc
@@ -41,3 +41,45 @@ mpm::Polynomial::IntegralBase<2, 3, 16>::Square_Definite_Integrals =
 template <>
 const Eigen::Matrix<double, 27, 1>
 mpm::Polynomial::IntegralBase<3, 2, 27>::Square_Definite_Integrals = Eigen::Matrix<double, 27, 1>::Zero();
+
+//! 2D Triangle
+//! Polynomial order 1 and number of terms 4
+//! 4 monomials: 1, y, x, xy
+//! Integration of monomials over [0,0] x [1,1-x] unit triangle
+template <>
+const Eigen::Matrix<double, 4, 1>
+mpm::Polynomial::IntegralBase<2, 1, 4>::Tri_Definite_Integrals = (Eigen::MatrixXd(4,1) << 1. / 2., 1. / 6., 1. / 6., 1. / 24.).finished();
+
+//! 2D Triangle
+//! Polynomial order 2 and number of terms 9
+//! 9 monomials: 1, y, y^2, x, xy, xy^2, x^2, x^2 y, x^2 y^2
+//! Integration of monomials over [0,0] x [1,1-x] unit triangle
+template <>
+const Eigen::Matrix<double, 9, 1>
+    mpm::Polynomial::IntegralBase<2, 2, 9>::Tri_Definite_Integrals =
+        (Eigen::MatrixXd(9, 1) << 1. / 2., 1. / 6., 1. / 12., 1. / 6., 1. / 24.,
+         1. / 60., 1. / 12., 1. / 60., 1. / 180.)
+            .finished();
+
+//! 2D Triangle
+//! Polynomial order 3 and number of terms 16
+//! 16 monomials: 1, y, y^2, y^3, x, xy, xy^2, xy^3, x^2, x^2 y, x^2 y^2, x^2
+//! y^3, x^3, x^3 y, x^3 y^2, x^3 y^3
+//! Integration of monomials over [0,0] x [1,1-x] unit triangle
+template <>
+const Eigen::Matrix<double, 16, 1>
+    mpm::Polynomial::IntegralBase<2, 3, 16>::Tri_Definite_Integrals =
+        (Eigen::MatrixXd(16, 1) << 1. / 2., 1. / 6., 1. / 12., 1. / 20.,
+         1. / 6., 1. / 24., 1. / 60., 1. / 120., 1. / 12., 1. / 60., 1. / 180.,
+         1. / 420., 1. / 20., 1. / 120., 1. / 420., 1. / 1120.)
+            .finished();
+
+//! 3D Tetrahedron
+//! Polynomial order 2 and number of terms 27
+//! 27 monomials: 1, z, z^2, y, yz, yz^2, y^2, y^2 z, y^2 z^2, x, xz, x z^2, x
+//! y, xyz, xyz^2, xy^2, xy^2 z, xy^2 z^2, x^2, x^2 z, x^2 z^2, x^2 y, x^2 yz,
+//! x^2 yz^2, x^2 y^2, y^2 z, x^2 y^2 z^2
+// Integration of monomials over [0,0,0] x [0, 1-x, 1-x-y]
+template <>
+const Eigen::Matrix<double, 27, 1>
+mpm::Polynomial::IntegralBase<3, 2, 27>::Tri_Definite_Integrals = Eigen::Matrix<double, 27, 1>::Zero();
