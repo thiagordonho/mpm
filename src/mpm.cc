3eab7a4471fbaa1b172bbf03767fa21cccce7e50
shyamini <nskshyamini@gmail.com>
2019-06-25 15:44:47 -0700

    :computer: :construction: Renamde explicit two phase to fsm two phase

--- src/mpm.cc
+++ src/mpm.cc
@@ -4,7 +4,7 @@
 #include "io.h"
 #include "mpm.h"
 #include "solvers/mpm_explicit.h"
-#include "solvers/mpm_explicit_twophase.h"
+#include "solvers/mpm_fsm_twophase.h"
 
 // 2D Explicit MPM USF
 static Register<mpm::MPM, mpm::MPMExplicit<2>, std::unique_ptr<mpm::IO>&&>
@@ -14,12 +14,10 @@ static Register<mpm::MPM, mpm::MPMExplicit<2>, std::unique_ptr<mpm::IO>&&>
 static Register<mpm::MPM, mpm::MPMExplicit<3>, std::unique_ptr<mpm::IO>&&>
     mpm_explicit_3d("MPMExplicit3D");
 
-// 2D Explicit Two Phase MPM
-static Register<mpm::MPM, mpm::MPMExplicitTwoPhase<2>,
-                std::unique_ptr<mpm::IO>&&>
-    mpm_explicit_twophase_2d("MPMExplicitTwoPhase2D");
+// 2D Fractional Step Method Two Phase MPM
+static Register<mpm::MPM, mpm::MPMFSMTwoPhase<2>, std::unique_ptr<mpm::IO>&&>
+    mpm_fsm_twophase_2d("MPMFSMTwoPhase2D");
 
-// 3D Explicit Two Phase MPM
-static Register<mpm::MPM, mpm::MPMExplicitTwoPhase<3>,
-                std::unique_ptr<mpm::IO>&&>
-    mpm_explicit_twophase_3d("MPMExplicitTwoPhase3D");
+// 3D Fractional Step Method Two Phase MPM
+static Register<mpm::MPM, mpm::MPMFSMTwoPhase<3>, std::unique_ptr<mpm::IO>&&>
+    mpm_fsm_twophase_3d("MPMFSMTwoPhase3D");
